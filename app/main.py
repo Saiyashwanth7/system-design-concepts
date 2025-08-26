01183ad44d6e57b3e05ce37b7831b3dcefcad90f
Shuchasmitha <saiyashwanthdasari@gmail.com>
2025-08-26 11:48:55 +0530

    Load Balancing and Reverse Proxy

--- app/main.py
+++ app/main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 import socket
+
 app=FastAPI()
+
+@app.middleware('http')
+async def sample_middleware(request:Request,call_next):
+    print("Before middleware")
+    response=await call_next(request)
+    print("after middle")
+    return response
 
 @app.get('/')
 async def root():
